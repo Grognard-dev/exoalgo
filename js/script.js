fa74496d89b8e14bd591e097f2731f11493b589d
Grognard-dev <lefevre.gregoire19@gmail.com>
2020-03-17 20:46:42 +0100

    Exercice afficher dans un formulaire

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var formulaire = document.createElement('FORM');
+var mabox=document.createElement('INPUT');
+mabox.type='TEXT';
+mabox.name='montext';
+mabox.value='Je suis une petite perruche';
+formulaire.appendChild(mabox);
+document.body.appendChild(formulaire);
